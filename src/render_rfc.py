f15f0d91d9e9f8ac3a949cc1f3fc20b3ee4f2a80
Karim Hamidou <karim@MacBook.local>
2016-03-04 08:25:33 -0800

    better regexps

--- src/render_rfc.py
+++ src/render_rfc.py
@@ -19,15 +19,23 @@ def remove_page_breaks(data):
     return data
 
 
+def create_paragraphs(data):
+    paragraph_regexp = re.compile(r"^((\w|\"|\').+(\.|\:))\n\n", re.DOTALL)
+    data = paragraph_regexp.sub(r'<p>\1</p>', data)
+    return data
+
+
 def anchor_titles(data):
-    lvl1_title_rx = re.compile(r"^(\d+\.)(.*)$", re.MULTILINE)
-    data = lvl1_title_rx.sub(r'\t<a name="\1"><h2>\1 \2</h2></a>', data)
+    # Reverse section order because lvl1_title_rx matches the beginning of lvl2_title_rx
+    # and lvl3_title_rx.
+    lvl3_title_rx = re.compile(r"^(\d+)\.(\d+)\.(\d+)\.(.*)$", re.MULTILINE)
+    data = lvl3_title_rx.sub(r'\t<a name="section-\1.\2.\3"><h4>\1.\2.\3 \4</h4></a>', data)
 
-    lvl2_title_rx = re.compile(r"^(\d+\.\d+\.)(.*)$", re.MULTILINE)
-    data = lvl2_title_rx.sub(r'\t<a name="\1"><h3>\1 \2</h3></a>', data)
+    lvl2_title_rx = re.compile(r"^(\d+)\.(\d+)\.(.*)$", re.MULTILINE)
+    data = lvl2_title_rx.sub(r'\t<a name="section-\1.\2"><h3>\1.\2 \3</h3></a>', data)
 
-    lvl3_title_rx = re.compile(r"^(\d+\.\d+\.\d+\.)(.*)$", re.MULTILINE)
-    data = lvl3_title_rx.sub(r'\t<a name="\1"><h4>\1 \2</h4></a>', data)
+    lvl1_title_rx = re.compile(r"^(\d+)\.(.*)$", re.MULTILINE)
+    data = lvl1_title_rx.sub(r'\t<a name="section-\1"><h2>\1. \2</h2></a>', data)
 
     return data
 
@@ -37,7 +45,8 @@ def render_rfc(filename):
         data = fd.read()
 
     out = data
-    for fn in [remove_top_space, remove_page_breaks, anchor_titles]:
+    for fn in [remove_top_space, remove_page_breaks,
+               anchor_titles, create_paragraphs]:
         out = fn(out)
 
     env = Environment(loader=FileSystemLoader('templates'))
@@ -45,8 +54,8 @@ def render_rfc(filename):
 
     dct = dict(rfc=out)
     rendered = template.render(**dct)
-    # return rendered
-    return out
+    return rendered
+    # return out
 
 
 def main():
